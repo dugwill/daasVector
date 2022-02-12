f70d8b58b4b878f2773a8ab9ab5d8f6cbc587c4d
dugwill <douglaswi1@comcast.net>
2022-02-12 07:28:37 -0700

    remove http code

--- daasVector.go
+++ daasVector.go
@@ -5,16 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
-	"time"
 )
 
 func main() {
 
-	var jsonData = []byte(`{
-		"name": "morpheus",
-		"job": "leader"
-	}`)
+	var jsonData = []byte(`{"name": "morpheus","job": "leader"}`)
 
 	c, err := NewSocket("tcp", "127.0.0.1:9000")
 	if err != nil {
@@ -22,39 +17,6 @@ func main() {
 	}
 
 	fmt.Fprintln(c, bytes.NewBuffer(jsonData))
-
-	// wait for reply
-	//message, _ := bufio.NewReader(c).ReadString('\n')
-	//fmt.Print("Message from server: " + message)
-
-}
-
-// NewClient crates a new http client with the provided timeout.
-// If redirects is true, the request will follow any redirects in the response
-func NewClient(t time.Duration, redirects bool) (client *http.Client) {
-	if redirects {
-		client = &http.Client{
-			Timeout: t * time.Millisecond,
-		}
-	} else {
-		client = &http.Client{
-			Timeout: t * time.Millisecond,
-			// Do not Follow redirect
-			CheckRedirect: func(req *http.Request, via []*http.Request) error { return http.ErrUseLastResponse },
-		}
-	}
-	return client
-}
-
-func NewRequest(url string, d []byte) (*http.Request, error) {
-	// Create Request
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(d))
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("User-Agent", "scte35Mon/1.0")
-	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
-	return req, err
 }
 
 func NewSocket(n, a string) (conn net.Conn, err error) {
